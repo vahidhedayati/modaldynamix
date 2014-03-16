8292c4f8f735208c67a8a909f263344a3503d3d1
Vahid <vahid.hedayati@gmail.com>
2014-03-16 15:14:24 +0000

    updates

--- grails-app/taglib/modaldynamix/ModalDynamixTagLib.groovy
+++ grails-app/taglib/modaldynamix/ModalDynamixTagLib.groovy
@@ -13,6 +13,8 @@ class ModalDynamixTagLib {
 	 *(notice the extra tag loadbootstrap above)
 	 *In your main file to run in overall site 
 	 *OR just call this on a specific gsp page if you have specific use
+	 *
+	 * <g:loadbootrap/> 
 	 */
 	 
 	def loadbootstrap= {
@@ -20,6 +22,13 @@ class ModalDynamixTagLib {
 	}
 	
 	
+	/*
+	 * g:getModalButton
+	 * <g:getModalButton divId="something" id="MATCHIDSETFOR_modalForm_BELOW"  title="hover title" value="click my button"/> 
+	 * 
+	 * Loads up button that triggers your modalbox containing forms or whatever
+	 * 
+	 */
 	def genModalButton= { attrs,body->
 			
 		/*
@@ -57,23 +66,23 @@ class ModalDynamixTagLib {
 			
 			attrs.value='CLICK ME'
 		}
-		out << """
-			<button href="#${attrs.id}" class="btn btn-block btn-success" 
-		role="button" data-toggle="modal"  onclick="run${attrs.id}()" title="${attrs.title}">
- 		${attrs.value}</button>
 
-			<script type="text/javascript">
- 	 		function run${attrs.id}() {
- 	 	 		\$('#${attrs.divId}').show();
- 	 		}
- 	 	    </script>
-           """
-		
+		out << g.render(contextPath: pluginContextPath,template: 'loadmodalbutton', model:[attrs:attrs])
 	}
 	
 
+	
+	
+	/*
+	 * modalForm call 
+	 * ==========================
+	 * 
+	 * This carries out all the form control
+	 * 
+	 * 
+	 * 
+	 */
 	def modalForm={attrs,body ->
-		
 		
 		/*
 		 * Attribute: close
@@ -86,26 +95,22 @@ class ModalDynamixTagLib {
 			attrs.close='X'
 		}
 		
-		
-		
-		
-		
 		/*
 		 *  Attribute: ModalTemplate
-		 *  
+		 *
 		 *  This calls the actual modalbox required to process your action.
 		 *  The supported plugin calls are:
-		 *  
+		 *
 		 *   Iframe: '/modaldynamix/modaliframe'
 		 *   Remote Form: '/modaldynamix/modalRemoteForm'
 		 *   Self Posting: form: '/modaldynamix/modalSelfPostForm'
-		 *   Basic Form: '/modaldynamix/modalbasic' 
-		 *   
-		 *   
-		 *   If you have noticed the attribute is given a URI to the template within plugin, 
-		 *   this of course can easily be overridden by your a call to a new modalbox call 
-		 *   within your own application  
-		 *  		 
+		 *   Basic Form: '/modaldynamix/modalbasic'
+		 *
+		 *
+		 *   If you have noticed the attribute is given a URI to the template within plugin,
+		 *   this of course can easily be overridden by your a call to a new modalbox call
+		 *   within your own application
+		 *
 		 */
 		if (!attrs.modalTemplate) {
 			throwTagError("Tag [modalForm] is missing required attribute [modalTemplate]")
@@ -121,45 +126,86 @@ class ModalDynamixTagLib {
 		 *
 		 *  so if you chose:
 		 * =====================================================================
-		 * 
+		 *
 		 *  modalTemplate='/modaldynamix/modalRemoteForm' (Using Remote Form)
-		 *  
+		 *
 		 *  Your modalTemplatePage would call the default grails _forms.gsp
 		 *  which contains just the fields for the form
 		 *
 		 *  modalTemplatePage="/myLocalDomain/form"
 		 *
 		 * =====================================================================
-		 *  
+		 *
 		 *  modalTemplate='/modaldynamix/modaliframe' (using iframes) OR
 		 *  modalTemplate='/modaldynamix/modalSelfPostForm' (using SelfPost Form):
-		 *  
+		 *
 		 *  Your modalTemplatePage would call a template within your application
 		 *  that contains a complete form with all tags for the form, in this example
 		 *  I have called formSelfPost from the demo site which is a full form
 		 *
 		 *  modalTemplatePage="/myLocalDomain/formSelfPost"
 		 * =====================================================================
-		 *  
+		 *
 		 * Just remember this is a template provided by your own application
-		 *   
+		 *
 		 */
 		
 		if (!attrs.modalTemplatePage) {
 			throwTagError("Tag [modalForm] is missing required attribute [modalTemplate]")
 		}
 		
+		/*
+		 * Attribute: id
+		 * This must be set to be the same value as defined in genModalButton: Id
+		 */
+		if (!attrs.id) {
+			throwTagError("Tag [modalForm] is missing required attribute [id] (must be set to be the same value as defined in genModalButton: Id)")
+		}
+		
+		
+		
+		
+		
+		// Specific checks for modalRemoteForm call within plugin:
+		if (attrs.modalTemplate.equals('/modaldynamix/modalRemoteForm')) {
+			VerifyRemoteFormAttrs(attrs)
+		}
+		
+		// Specific configuration + checks for modaliframe call within plugin:
+		if (attrs.modalTemplate.equals('/modaldynamix/modaliframe')) {
+			verifyIframe(attrs)
+		}	
+		
+		// Specific configuration + checks for modalSelfPostForm call within plugin:
+		if (attrs.modalTemplate.equals('/modaldynamix/modalSelfPostForm')) {
+			verifyGeneral(attrs)
+		}
+		
+		out << g.render(contextPath: pluginContextPath,template: attrs.modalTemplate, model: [attrs:attrs])
+		
+	}
 	
+	
+	private void verifyGeneral(attrs) {
+		
+		/*
+		 * Attribute: divId
+		 * This is the div id value that contains your calls below, so its pure name
+		 */
+		if (!attrs.divId) {
+			throwTagError("Tag [modalForm] is missing required attribute [divId] ( div id value that contains your calls below")
+		}
+		
 		/*
 		 * Attribute: queryController
 		 * This by default calls modaldynamix controller within this plugin
 		 *
 		 * It simply carries out calls to return information from your own application
-		 * 
+		 *
 		 * You can override this by setting:
 		 *  queryController='MyController'
-		 * 
-		 * 
+		 *
+		 *
 		 */
 		if (!attrs.queryController) {
 			attrs.queryController="modaldynamix"
@@ -172,10 +218,10 @@ class ModalDynamixTagLib {
 		 *
 		 * It simply carries out calls to return templates from within your own application.
 		 * These two actions basically show :
-		 * 
+		 *
 		 * Your_DivId_[Display]
 		 * Your_DivId_[Form]
-		 * 
+		 *
 		 * Templates that you have already defined on the main page doing the call
 		 *
 		 * You can override this by setting:
@@ -184,33 +230,9 @@ class ModalDynamixTagLib {
 		 *
 		 */
 		
-		if (!attrs.queryAction) { 
+		if (!attrs.queryAction) {
 			attrs.queryAction="getTemplate"
 		}
-		
-		
-		
-		// Specific checks for modalRemoteForm call within plugin:
-		if (attrs.modalTemplate.equals('/modaldynamix/modalRemoteForm')) {
-			VerifyRemoteFormAttrs(attrs)
-		}
-		
-		// Specific configuration + checks for modaliframe call within plugin:
-		if (attrs.modalTemplate.equals('/modaldynamix/modaliframe')) {
-			verifyIframe(attrs)
-			
-			
-		
-		}	
-		
-		
-		out << g.render(contextPath: pluginContextPath,template: attrs.modalTemplate, model: [attrs:attrs])
-		
-	}
-	
-	
-	
-	private void verifyIframe(attrs) {
 		
 		/*
 		 * Attribute: modalJsTemplate
@@ -225,6 +247,40 @@ class ModalDynamixTagLib {
 		
 		attrs.modalJsTemplate='/modaldynamix/modal-js'
 		
+		/*
+		 * Attribute: formId
+		 * This by default will set it as [ID]FORM
+		 *
+		 * Its the form name/id
+		 *
+		 * You can override this by setting:
+		 *  queryiController='MyController'
+		 *
+		 *
+		 */
+		
+		if (!attrs.formId) {
+			attrs.formId="${attrs.id}-FORM"
+		}
+		
+		
+		/*   Attribute: returnController
+		 * 
+		 *  This is the current controller that is serving this taglib call
+		 *  So where ever this call is being made from get the controller that is being used to serve it
+		 *  		 
+		 */
+		if (!attrs.returnController) {
+			throwTagError("Tag [modalForm] is missing required attribute [returnController] - current controller that is serving this taglib call")
+		}
+		
+		
+		
+	}
+	
+	private void verifyIframe(attrs) {
+		
+		verifyGeneral(attrs)
 		 
 		/*
 		 * Attribute: iController
@@ -273,22 +329,7 @@ class ModalDynamixTagLib {
 	}
 	
 	private void VerifyRemoteFormAttrs(attrs){
-		
-	
-		
-		/*
-		 * Attribute: modalJsTemplate
-		 *
-		 * ONLY REQUIRED IF: RemoteForm/modaliframe Template is defined
-		 *
-		 * This simply sets the value of js file to call on backend plugin
-		 * which gets added to work with when user clicks close or form remotely actions
-		 * + then closes.
-		 *
-		 */
-		
-		attrs.modalJsTemplate='/modaldynamix/modal-js'
-	  
+		verifyGeneral(attrs)
 	  /*
 	   * Attribute: submitValue
 	   *
---
+++ grails-app/views/_loadmodalbutton.gsp
@@ -0,0 +1,16 @@
+
+	<button href="#${attrs.id}" 
+		class="btn btn-block btn-success" 
+		role="button" data-toggle="modal"  
+		onclick="run${attrs.id}()" 
+		title="${attrs.title}">
+		
+ 	${attrs.value}
+ 	
+ 	</button>
+
+<script type="text/javascript">
+	function run${attrs.id}() {
+ 		$('#${attrs.divId}').show();
+ 	 }
+</script>
